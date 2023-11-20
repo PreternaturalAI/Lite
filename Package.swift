5e8968ce0129e6ebf0184628f0f30ec57a41e123
Vatsal Manot <vatsal.manot@yahoo.com>
2023-11-19 20:04:41 -0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version:5.9
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+    name: "Lite",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "Lite",
+            targets: [
+                "Lite"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
+        .package(url: "https://github.com/SwiftUIX/SwiftUIZ.git", branch: "main"),
+        .package(url: "https://github.com/vmanot/CorePersistence.git", branch: "main"),
+        .package(url: "https://github.com/vmanot/Expansions.git", branch: "main"),
+        .package(url: "https://github.com/vmanot/Merge.git", branch: "master"),
+        .package(url: "https://github.com/vmanot/Swallow.git", branch: "master"),
+    ],
+    targets: [
+        .macro(
+            name: "LiteMacros",
+            dependencies: [
+                "Expansions",
+                "Swallow",
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftOperators", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ],
+            path: "Sources/LiteMacros"
+        ),
+        .target(
+            name: "Lite",
+            dependencies: [
+                "CorePersistence",
+                "Expansions",
+                "Merge",
+                "Swallow",
+                "SwiftUIX",
+                "SwiftUIZ",
+            ],
+            path: "Sources/Lite"
+        )
+    ]
+)
